47232b2f2c26f007b509dcfa7eead4fb1b4125af
futonkris <141852937+futonkris@users.noreply.github.com>
2025-02-15 17:51:02 +0800

    extension stuff

---
+++ reddit-extension/public/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn');
+    if (btn) {
+      btn.addEventListener('click', () => {
+        console.log('joe mama');
+      });
+    }
+  });   
---
+++ reddit-extension/src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <main>
+        <div id="message">
+          <span>Hello world</span>
+        </div>
+        <button id="btn">Click me</button>
+      </main>
+    );
+  } 
